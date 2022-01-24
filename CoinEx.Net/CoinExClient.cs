42df203cb76510e240a7a93a5a71bdb938abb582
m.bigdeli <majidbigdeli72@gmail.com>
2022-01-24 06:35:14 -0800

    change GetUserTrade

--- CoinEx.Net/CoinExClient.cs
+++ CoinEx.Net/CoinExClient.cs
@@ -674,14 +674,21 @@ namespace CoinEx.Net
         /// <returns>List of trades for a symbol</returns>
         public async Task<WebCallResult<CoinExPagedResult<CoinExOrderTradeExtended>>> GetUserTradesAsync(string symbol, int page, int limit, CancellationToken ct = default)
         {
-            symbol.ValidateCoinExSymbol();
             limit.ValidateIntBetween(nameof(limit), 1, 100);
+
             var parameters = new Dictionary<string, object>
             {
-                { "market", symbol },
                 { "page", page },
                 { "limit", limit }
             };
+
+            if (!string.IsNullOrWhiteSpace(symbol))
+            {
+                if (!Regex.IsMatch(symbol, "^([0-9A-Z]{5,})$"))
+                    throw new ArgumentException($"{symbol} is not a valid CoinEx symbol. Should be [QuoteCurrency][BaseCurrency], e.g. ETHBTC");
+
+                parameters.Add("market", symbol);
+            }
 
             return await ExecutePaged<CoinExOrderTradeExtended>(GetUrl(UserTransactionsEndpoint), HttpMethod.Get, ct, parameters, true).ConfigureAwait(false);
         }
