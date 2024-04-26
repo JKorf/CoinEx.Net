e02c4b9292489bd1cd7794bb10d683ad746dcc43
JKorf <jankorf91@gmail.com>
2024-04-26 15:40:34 +0200

    wip

---
+++ CoinEx.Net/CoinExExchange.cs
@@ -0,0 +1,26 @@
+﻿namespace CoinEx.Net
+{
+    /// <summary>
+    /// CoinEx exchange information and configuration
+    /// </summary>
+    public static class CoinExExchange
+    {
+        /// <summary>
+        /// Exchange name
+        /// </summary>
+        public static string ExchangeName => "CoinEx";
+
+        /// <summary>
+        /// Url to the main website
+        /// </summary>
+        public static string Url { get; } = "https://www.coinex.com";
+
+        /// <summary>
+        /// Urls to the API documentation
+        /// </summary>
+        public static string[] ApiDocsUrl { get; } = new[] {
+            "https://viabtc.github.io/coinex_api_en_doc/",
+            "https://docs.coinex.com/api/v2/"
+            };
+    }
+}
