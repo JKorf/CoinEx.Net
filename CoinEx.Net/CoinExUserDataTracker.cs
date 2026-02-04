96ff81e1acecf5e7a4cdf9c80079dfca1345171f
Jkorf <jankorf91@gmail.com>
2026-02-04 16:32:16 +0100

    wip

---
+++ CoinEx.Net/CoinExUserDataTracker.cs
@@ -0,0 +1,63 @@
+﻿using CoinEx.Net.Interfaces.Clients;
+using CryptoExchange.Net.SharedApis;
+using CryptoExchange.Net.Trackers.UserData;
+using CryptoExchange.Net.Trackers.UserData.Objects;
+using Microsoft.Extensions.Logging;
+
+namespace CoinEx.Net
+{
+    /// <inheritdoc/>
+    public class CoinExUserSpotDataTracker : UserSpotDataTracker
+    {
+        /// <summary>
+        /// ctor
+        /// </summary>
+        public CoinExUserSpotDataTracker(
+            ILogger<CoinExUserSpotDataTracker> logger,
+            ICoinExRestClient restClient,
+            ICoinExSocketClient socketClient,
+            string? userIdentifier,
+            SpotUserDataTrackerConfig config) : base(
+                logger,
+                restClient.SpotApiV2.SharedClient,
+                null,
+                restClient.SpotApiV2.SharedClient,
+                socketClient.SpotApiV2.SharedClient,
+                restClient.SpotApiV2.SharedClient,
+                socketClient.SpotApiV2.SharedClient,
+                socketClient.SpotApiV2.SharedClient,
+                userIdentifier,
+                config)
+        {
+        }
+    }
+
+    /// <inheritdoc/>
+    public class CoinExUserFuturesDataTracker : UserFuturesDataTracker
+    {
+        /// <inheritdoc/>
+        protected override bool WebsocketPositionUpdatesAreFullSnapshots => false;
+
+        /// <summary>
+        /// ctor
+        /// </summary>
+        public CoinExUserFuturesDataTracker(
+            ILogger<CoinExUserFuturesDataTracker> logger,
+            ICoinExRestClient restClient,
+            ICoinExSocketClient socketClient,
+            string? userIdentifier,
+            FuturesUserDataTrackerConfig config) : base(logger,
+                restClient.FuturesApi.SharedClient,
+                null,
+                restClient.FuturesApi.SharedClient,
+                socketClient.FuturesApi.SharedClient,
+                restClient.FuturesApi.SharedClient,
+                socketClient.FuturesApi.SharedClient,
+                socketClient.FuturesApi.SharedClient,
+                socketClient.FuturesApi.SharedClient,
+                userIdentifier,
+                config)
+        {
+        }
+    }
+}
--- CoinEx.Net/Interfaces/ICoinExTrackerFactory.cs
+++ CoinEx.Net/Interfaces/ICoinExTrackerFactory.cs
@@ -1,7 +1,10 @@
-﻿using CryptoExchange.Net.Interfaces;
+﻿using CryptoExchange.Net.Authentication;
+using CryptoExchange.Net.Interfaces;
 using CryptoExchange.Net.SharedApis;
 using CryptoExchange.Net.Trackers.Klines;
 using CryptoExchange.Net.Trackers.Trades;
+using CryptoExchange.Net.Trackers.UserData.Interfaces;
+using CryptoExchange.Net.Trackers.UserData.Objects;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -13,5 +16,32 @@ namespace CoinEx.Net.Interfaces
     /// </summary>
     public interface ICoinExTrackerFactory : ITrackerFactory
     {
+        /// <summary>
+        /// Create a new Spot user data tracker
+        /// </summary>
+        /// <param name="userIdentifier">User identifier</param>
+        /// <param name="config">Configuration</param>
+        /// <param name="credentials">Credentials</param>
+        /// <param name="environment">Environment</param>
+        IUserSpotDataTracker CreateUserSpotDataTracker(string userIdentifier, SpotUserDataTrackerConfig config, ApiCredentials credentials, CoinExEnvironment? environment = null);
+        /// <summary>
+        /// Create a new spot user data tracker
+        /// </summary>
+        /// <param name="config">Configuration</param>
+        IUserSpotDataTracker CreateUserSpotDataTracker(SpotUserDataTrackerConfig config);
+
+        /// <summary>
+        /// Create a new futures user data tracker
+        /// </summary>
+        /// <param name="userIdentifier">User identifier</param>
+        /// <param name="config">Configuration</param>
+        /// <param name="credentials">Credentials</param>
+        /// <param name="environment">Environment</param>
+        IUserFuturesDataTracker CreateUserFuturesDataTracker(string userIdentifier, FuturesUserDataTrackerConfig config, ApiCredentials credentials, CoinExEnvironment? environment = null);
+        /// <summary>
+        /// Create a new futures user data tracker
+        /// </summary>
+        /// <param name="config">Configuration</param>
+        IUserFuturesDataTracker CreateUserFuturesDataTracker(FuturesUserDataTrackerConfig config);
     }
 }
