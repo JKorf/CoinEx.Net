c0c4312dcf914f94496c605e8ded5344d217a54f
Jkorf <jankorf91@gmail.com>
2026-06-02 09:15:05 +0200

    Fixed user client provider not caching new client when previous client was disposed

--- CoinEx.Net/Clients/CoinExUserClientProvider.cs
+++ CoinEx.Net/Clients/CoinExUserClientProvider.cs
@@ -89,7 +89,7 @@ namespace CoinEx.Net.Clients
             if (credentials != null)
             {
                 client.SetApiCredentials(credentials);
-                _restClients.TryAdd(userIdentifier, client);
+                _restClients[userIdentifier] = client;
             }
             return client;
         }
@@ -101,7 +101,7 @@ namespace CoinEx.Net.Clients
             if (credentials != null)
             {
                 client.SetApiCredentials(credentials);
-                _socketClients.TryAdd(userIdentifier, client);
+                _socketClients[userIdentifier] = client;
             }
             return client;
         }
